a816a8079a7300b3b64eafa1c5e984ca69105b03
Ayman El Harzli <aymanmoschino@gmail.com>
2025-12-07 05:54:54 +0100

    fix: small fix where I forgot to call the features section in the parent container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import Hero from "./Sections/Hero";
 import ServicesOptimized from "./Sections/ServicesOptimized";
+import FeaturedProperties from "./Sections/FeaturedProperties"
 
 function Home() {
   return (
     <>
       <Hero />
       <ServicesOptimized />
+      <FeaturedProperties />
+
     </>
   );
 }
